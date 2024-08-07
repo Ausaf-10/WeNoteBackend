8914a2a5cdeb258f3b7c337cfe5b42d63e3c5ee9
Ausaf Aslam <102360335+Ausaf-10@users.noreply.github.com>
2024-08-08 01:26:42 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require('dotenv').config()
+const db_link = `mongodb+srv://${process.env.MongoUser}:${process.env.MongoPassword}@cluster0.odskqtk.mongodb.net/`;
+connectToMongo = ()=>{
+    mongoose.connect(db_link).then(()=>{
+        console.log("Connected");
+    }).catch((err)=>{
+        console.log(err);
+        console.log("Unable to connect to the database");
+    })
+}
+module.exports = connectToMongo;
